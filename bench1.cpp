0f0e2ad6b735dd2f724168ae746f53e79cb1a17d
алина <agnistova1@gmail.com>
2024-10-28 17:10:08 +0300

    done bench1

---
+++ bench1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+//ema-search-str поиск подстроки в тексте во внешней памяти
+//По алгоритму Бойера-Мура
+#define NO_OF_CHARS 256 //кол-во возможных символов (256 для ASCII)
+
+void bad_char_heuristic(const string &pattern, vector<int> &bad_char) {
+    int length = pattern.length();
+    for (int i = 0; i < length; i++) {
+        bad_char[static_cast<int>(pattern[i])] = i;
+    }
+}
+
+void boyer_moor_search(string &txt, string &pattern) {
+    int m = pattern.length();
+    int n = txt.length();
+    vector<int> bad_char(NO_OF_CHARS, -1);
+    bad_char_heuristic(pattern, bad_char);
+    int s = 0;
+    while (s <= n - m) {
+        int j = m - 1;
+        while (j >= 0 && pattern[j] == txt[s + j]) {
+            j--;
+        }
+        if (j < 0) {
+            cout << "Pattern occurs at shift =  " << s << '\n';
+            s += (s + m < n) ? m - bad_char[static_cast<int>(txt[s + m])] : 1;
+        } else {
+            s += max(1, j - bad_char[static_cast<int>(txt[s + j])]);
+        }
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
+        cerr << "Usage: " << argv[0] << " <file_path> <pattern> <repeat_count>\n";
+        return 1;
+    }
+    string path_to_file = argv[1];
+    string pattern = argv[2];
+    int repeat_count = stoi(argv[3]);
+    ifstream file(path_to_file);
+    if (!file) {
+        cerr << "Error opening file" << '\n';
+        return 1;
+    }
+    string txt;
+    string line;
+    while (getline(file, line)) {
+        txt += line + "\n";
+    }
+    file.close();
+
+    auto start_time = chrono::high_resolution_clock::now();
+    for (int i = 0; i < repeat_count; ++i) {
+        boyer_moor_search(txt, pattern);
+    }
+
+    auto end_time = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end_time - start_time;
+    cout << "Total execution time: " << elapsed.count() << " seconds\n";;
+    return 0;
+}
